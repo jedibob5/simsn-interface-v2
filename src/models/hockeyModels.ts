609ce789c0c758130a436dc947308ce67c4e3cca
Kirbskol <138513974+Kirbskol@users.noreply.github.com>
2025-04-03 12:30:14 +0100

    Add properties to bootstrap model for bba and hck for contract data

--- src/models/hockeyModels.ts
+++ src/models/hockeyModels.ts
@@ -3178,6 +3178,8 @@ export class BootstrapData {
   ProTeamLineups: ProfessionalLineup[];
   ProTeamShootoutLineup: ProfessionalShootoutLineup;
   FaceData: { [key: number]: FaceDataResponse };
+  ContractMap: { [key: number]: ProContract };
+  ExtensionMap: { [key: number]: ExtensionOffer };
 
   constructor(source: any = {}) {
     if ("string" === typeof source) source = JSON.parse(source);
@@ -3270,6 +3272,16 @@ export class BootstrapData {
     this.FaceData = this.convertValues(
       source["FaceData"],
       FaceDataResponse,
+      true
+    );
+    this.ContractMap = this.convertValues(
+      source["ContractMap"],
+      ProContract,
+      true
+    );
+    this.ExtensionMap = this.convertValues(
+      source["ExtensionMap"],
+      ExtensionOffer,
       true
     );
   }
